542c83046353a0be19bf563085d5d71f8ecc2b96
sandy <81100360+sr-dev-coder@users.noreply.github.com>
2024-10-19 23:54:07 +0530

    feat(backedn): added workspaces tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,7 +3,12 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 const schema = defineSchema({
-    ...authTables
+    ...authTables,
+    workspaces: defineTable({
+        name: v.string(),
+        userId: v.id("users"),
+        joinCode: v.string()
+    })
 });
 
 export default schema;
